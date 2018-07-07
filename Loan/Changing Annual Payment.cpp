ee3f4a6cfad849e0576d015cde24e4da26b01390
David Li <31947328+David-SF2290@users.noreply.github.com>
2018-07-07 06:46:30 -0400

    Update Changing Annual Payment.cpp

--- Loan/Changing Annual Payment.cpp
+++ Loan/Changing Annual Payment.cpp
@@ -26,70 +26,68 @@ int endC = 4; /* column number for period ending balance       */
 
 for (int i = 1; i <= 5; i++) 
 {    
-     cout << "Enter periodic interest rate in decimal: ";
-     cin >> intRate;
+  cout << "Enter periodic interest rate in decimal: ";
+  cin >> intRate;
 
-     if ((intRate < 0.0) || (intRate > 0.15)) 
-     {    
-         if (i == 5) 
-         {
-             cout << "\nThis program is being terminated\n";
-             system("PAUSE");
-             return 0; 
-          } 
-         else 
-          {
-             cout << "You must enter a value between ";
-             cout << "0.0 and 0.15. Please try again." << endl; 
-          } 
-          }
-         else { break; } 
+if ((intRate < 0.0) || (intRate > 0.15)) 
+{    
+if (i == 5) 
+{
+  cout << "\nThis program is being terminated\n";
+  return 0; 
+} 
+else 
+{
+  cout << "You must enter a value between ";
+  cout << "0.0 and 0.15. Please try again." << endl; 
+} 
+}
+else { break; } 
 }
 		
 		
 for (int i = 1; i <= 5; i++) 
 {
-    cout << "Enter periodic jumpSize1 in decimal: ";
-    cin >> jumpSize1;
+  cout << "Enter periodic jumpSize1 in decimal: ";
+  cin >> jumpSize1;
 
-    if ((jumpSize1 < 0.0) || (jumpSize1 > 0.3)) 
-    {
-       if (i == 5) 
-       {
-       cout << "\nThis program is being terminated\n";
-       system("PAUSE");
-       return 0; 
-       } 
-       else
-       {
-       cout << "You must enter a value between ";
-       cout << "0.0 and 0.3. Please try again." << endl; 
-       } 
-       }
-       else { break; }
+if ((jumpSize1 < 0.0) || (jumpSize1 > 0.3)) 
+{
+if (i == 5) 
+{
+ cout << "\nThis program is being terminated\n";
+ return 0; 
+} 
+else
+{
+  cout << "You must enter a value between ";
+  cout << "0.0 and 0.3. Please try again." << endl; 
+} 
+}
+else { break; }
 }
 		
 		
 for (int i = 1; i <= 5; i++) 
 {
-    cout << "Enter periodic jumpSize2 in decimal: ";
-    cin >> jumpSize2;
+  cout << "Enter periodic jumpSize2 in decimal: ";
+  cin >> jumpSize2;
 
-    if ((jumpSize2 < 0.0) || (jumpSize2 > 0.3)) 
-    {
-        if (i == 5) 
-	{
-        cout << "\nThis program is being terminated\n";
-        system("PAUSE");
-        return 0; 
-	} 
-	else
-        {
-        cout << "You must enter a value between ";
-        cout << "0.0 and 0.3. Please try again." << endl; 
-	}
-    } 
-    else { break; } 
+if ((jumpSize2 < 0.0) || (jumpSize2 > 0.3)) 
+{
+if (i == 5) 
+{
+  cout << "\nThis program is being terminated\n";
+  system("PAUSE");
+  return 0; 
+} 
+else
+{
+  cout << "You must enter a value between ";
+  cout << "0.0 and 0.3. Please try again." << endl; 
+}
+} 
+else { break; } 
 }	
 		
 		
@@ -157,21 +155,21 @@ for(iter = 1; aT[loanLife -1][endC] >= tolerance; iter++)
 
     for(prd = 0; prd < loanLife; prd++)
     {
-        if (prd != 0) aT[prd][begC] = aT[prd -1][endC];
+    if (prd != 0) aT[prd][begC] = aT[prd -1][endC];
 
-           prdPmnt = initPmnt;
-           if(prd > jumpPrd1-2) prdPmnt = initPmnt * (1 + jumpSize1);
-               if(prd > jumpPrd2-2) prdPmnt = initPmnt * (1 + jumpSize1) * (1 + jumpSize2);
+      prdPmnt = initPmnt;
+      if(prd > jumpPrd1-2) prdPmnt = initPmnt * (1 + jumpSize1);
+      if(prd > jumpPrd2-2) prdPmnt = initPmnt * (1 + jumpSize1) * (1 + jumpSize2);
 
-               aT[prd][pmntC] = prdPmnt;
+        aT[prd][pmntC] = prdPmnt;
 
-               aT[prd][intC] = aT[prd][begC] * intRate;
-               aT[prd][prinC] = aT[prd][pmntC] - aT[prd][intC];
-               aT[prd][endC] = aT[prd][begC] - aT[prd][prinC];
+        aT[prd][intC] = aT[prd][begC] * intRate;
+        aT[prd][prinC] = aT[prd][pmntC] - aT[prd][intC];
+        aT[prd][endC] = aT[prd][begC] - aT[prd][prinC];
      }
 
-     stepSize = aT[loanLife-1][endC]/pow((1 + intRate), loanLife)/loanLife /(1+jumpSize1)/(1+jumpSize2);
-     cout << iter << ". " << aT[loanLife-1][endC] << endl;
+stepSize = aT[loanLife-1][endC]/pow((1 + intRate), loanLife)/loanLife /(1+jumpSize1)/(1+jumpSize2);
+cout << iter << ". " << aT[loanLife-1][endC] << endl;
 }
 
 /****************************************************************/
@@ -186,8 +184,6 @@ cout << aT[loanLife-1][endC] << endl << endl;
 printT(aT, loanLife);
 cout << endl <<endl;
 
-/*    Wait for the user to read the output on the console    */ 
-system("PAUSE");
 return 0;
 }
 
