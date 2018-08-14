b6280858ce264462c76b66b6f798352b7064433f
David Li <31947328+David-SF2290@users.noreply.github.com>
2018-08-14 08:11:23 -0400

    Create BacktestingSystem.py

---
+++ Backtesting/BacktestingSystem.py
@@ -0,0 +1,8 @@
+
+
+from BacktesterClass import Backtester
+
+backtester = Backtester("AAPL",dt.datetime(2014, 1, 1),
+                               dt.datetime(2014, 12, 31))
+
+backtester.start_backtest()                              
