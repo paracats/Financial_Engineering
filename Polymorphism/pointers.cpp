c77910ff438d3680c5c477f192157dff6b1e8107
David Li <31947328+David-SF2290@users.noreply.github.com>
2018-09-04 23:32:40 -0400

    Create pointers.cpp

---
+++ Polymorphism/pointers.cpp
@@ -0,0 +1,4 @@
+/* C++ For Quantitative Finance                                 */
+/* Using pointers with base & derived classes                   */
+/* Author: David Li                                             */
+
