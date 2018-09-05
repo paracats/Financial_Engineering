10650bf5a2c4659cc6989786157c16d2bb0ebb04
David Li <31947328+David-SF2290@users.noreply.github.com>
2018-09-05 11:33:15 -0400

    Create VirtualFunctions.cpp

---
+++ Polymorphism/VirtualFunctions.cpp
@@ -0,0 +1,17 @@
+/* C++ For Quantitative Finance                                 */
+/* Polymorphism with Virtual Functions                          */
+/* Author: David Li                                             */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
