a1935205642fd807774eafde53f7ed516aa5c403
David Li <31947328+David-SF2290@users.noreply.github.com>
2019-01-04 14:48:09 -0500

    Update WebCrawler.py

--- WebCrawler/WebCrawler/WebCrawler.py
+++ WebCrawler/WebCrawler/WebCrawler.py
@@ -1,1 +1,14 @@
 """ David Li               """
+""" Web Crawler            """
+
+
+from csv import reader
+from urllib import request
+from bs4 import BeautifulSoup
+from re import compile, IGNORECASE
+from json import dumps
+
+def load_web_page(url):
+    soup = BeautifulSoup(request.urlopen(url), 'html.parser')
+    page = soup.text
+    
