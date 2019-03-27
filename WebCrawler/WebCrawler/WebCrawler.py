ddb2817f4669b6fc5eab1eeac87ba51354875c04
David Li <31947328+David-SF2290@users.noreply.github.com>
2019-03-26 21:15:03 -0400

    Update WebCrawler.py

--- WebCrawler/WebCrawler/WebCrawler.py
+++ WebCrawler/WebCrawler/WebCrawler.py
@@ -39,6 +39,11 @@ def get_data_dict(text, name, time):
     """
     data['words_in_MDA'] = len(text_MDA.split()) - 2
 
+    """
+    The number of times the word “competition” 
+    is mentioned in the MD&A section
+    """
+    data['competition_in_MDA'] = text_MDA.lower().count('competition')
 
     
     
