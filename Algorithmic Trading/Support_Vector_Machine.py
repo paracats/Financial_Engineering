18ebb32507c46cd4182096bd4a27b8807e727597
David Li <31947328+David-SF2290@users.noreply.github.com>
2019-05-21 10:56:48 -0400

    Update Support_Vector_Machine.py

--- Algorithmic Trading/Support_Vector_Machine.py
+++ Algorithmic Trading/Support_Vector_Machine.py
@@ -34,15 +34,97 @@ class ReutersParser(HTMLParser):
         """
         HTMLParser.__init__(self)
         self._reset()
+        self.encoding = encoding
+      
+    def _reset(self):
+        """
+        This is called only on initialisation of the parser class
+        and when a new topic-body tuple has been generated. It
+        resets all off the state so that a new tuple can be subsequently
+        generated.
+        """
+        self.in_body = False
+        self.in_topics = False
+        self.in_topic_d = False
+        self.body = ""
+        self.topics = []
+        self.topic_d = ""  
+         
+   def parse(self, fd):
+        """
+        parse accepts a file descriptor and loads the data in chunks
+        in order to minimise memory usage. It then yields new documents
+        as they are parsed.
+        """
+        self.docs = []
+        for chunk in fd:
+            self.feed(chunk.decode(self.encoding))
+            for doc in self.docs:
+                yield doc
+            self.docs = []
+        self.close()
+
+   def handle_starttag(self, tag, attrs):
+        """
+        This method is used to determine what to do when the parser
+        comes across a particular tag of type "tag". In this instance
+        we simply set the internal state booleans to True if that particular
+        tag has been found.
+        """
+        if tag == "reuters":
+            pass
+        elif tag == "body":
+            self.in_body = True
+        elif tag == "topics":
+            self.in_topics = True
+        elif tag == "d":
+            self.in_topic_d = True  
+            
+      def handle_endtag(self, tag):
+          """
+          This method is used to determine what to do when the parser
+          finishes with a particular tag of type "tag".
+
+          If the tag is a <REUTERS> tag, then we remove all
+          white-space with a regular expression and then append the
+          topic-body tuple.
+
+          If the tag is a <BODY> or <TOPICS> tag then we simply set
+          the internal state to False for these booleans, respectively.
+
+          If the tag is a <D> tag (found within a <TOPICS> tag), then we
+          append the particular topic to the "topics" list and
+          finally reset it.
+          """
+          if tag == "reuters":
+              self.body = re.sub(r’\s+’, r’ ’, self.body)
+              self.docs.append( (self.topics, self.body) )
+              self._reset()
+          elif tag == "body":
+              self.in_body = False
+          elif tag == "topics":
+              self.in_topics = False
+          elif tag == "d":
+              self.in_topic_d = False
+              self.topics.append(self.topic_d)
+              self.topic_d = ""
+
+      def handle_data(self, data):
+          """
+          The data is simply appended to the appropriate member state
+          for that particular tag, up until the end closing tag appears.
+          """
+          if self.in_body:
+              self.body += data
+          elif self.in_topic_d:
+              self.topic_d += data      
 
 
-
-
-
-
-
-
-
+   def obtain_topic_tags():
+       """
+       Open the topic list file and import all of the topic names
+       taking care to strip the trailing "\n" from each word.
+       """
 
 
 
