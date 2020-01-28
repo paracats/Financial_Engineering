9ead10b6dcef637a5fa41b5bca0f69df7f985df3
Shaofei Li <31947328+David-SF2290@users.noreply.github.com>
2020-01-28 11:59:45 -0500

    Add files via upload

---
+++ Backtesting/TickDataClass.py
@@ -0,0 +1,13 @@
+""" Python For Quantitative Finance    """ 
+""" Tick Data Class                    """
+""" David Li                           """
+
+class TickData:
+    """ Store a single unit of data """
+    def __init__(self, symbol, timestamp,
+                 last_price=0, total_volume=0):
+        self.symbol = symbol
+        self.timestamp = timestamp
+        self.open_price = 0
+        self.last_price = last_price
+        self.total_volume = total_volume
