7974ac618bfd48e1908bd458423752e300524b29
Shaofei Li <31947328+David-SF2290@users.noreply.github.com>
2020-05-12 07:40:37 -0400

    Update 4.1.3 MarketDataSourceClass.py

--- 4. Backtesting/4.1.3 MarketDataSourceClass.py
+++ 4. Backtesting/4.1.3 MarketDataSourceClass.py
@@ -1,6 +1,6 @@
 """ Python For Quantitative Finance """ 
 """ MarketDataSourceClass           """
-""" David Li                        """
+""" Shaofei Li                      """
 
 import pandas.io.data as web
 
